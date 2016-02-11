45c81d5ff3a75854e92d2fdc958ec0dd43ce4d8b
saimkhan92 <saim.khan@colorado.edu>
2016-02-11 12:45:19 -0700

    first sort

---
+++ bubble_sort.py
@@ -0,0 +1,21 @@
+list_a=[3,1,5,5,6,7,6,3,2,5,9,4,3]
+print(list_a)
+print("bubble sort begins")
+listlen=len(list_a)
+
+while True:
+    j=0
+    while j<listlen-1:
+        if list_a[j]>list_a[j+1]:
+            temp=list_a[j]
+            list_a[j]=list_a[j+1]
+            list_a[j+1]=temp
+        j=j+1
+        print(list_a)
+    listlen=listlen-1
+    print(listlen)
+    if listlen==1:
+        break
+
+print("The sorted list is:",end=" ")
+print(list_a)
